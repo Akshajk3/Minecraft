fda91e7b8db7a6e736d15a1717498ec3718db987
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2026-04-28 01:53:23 -0700

    updated build to support mac

---
+++ Minecraft V2.0/build/Minecraft V2.0/CMakeFiles/CMakeTarget.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeTarget.dir/src/Main.cpp.o"
+  "CMakeFiles/CMakeTarget.dir/src/Main.cpp.o.d"
+  "CMakeFiles/CMakeTarget.dir/src/glad.c.o"
+  "CMakeFiles/CMakeTarget.dir/src/glad.c.o.d"
+  "CMakeTarget"
+  "CMakeTarget.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CMakeTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
