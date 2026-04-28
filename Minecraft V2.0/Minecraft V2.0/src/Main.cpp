2f258385bc58207fb1c9895cf944b1160aa3c6d8
Akshajk3 <78132834+Akshajk3@users.noreply.github.com>
2026-04-28 01:24:14 -0700

    Created Window

---
+++ Minecraft V2.0/Minecraft V2.0/src/Main.cpp
@@ -0,0 +1,46 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const unsigned int width = 800;
+const unsigned int height = 600;
+
+int main()
+{
+	if (!glfwInit()) {
+		std::cout << "Failed to init GLFW" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+
+	if (window == nullptr) {
+		std::cout << "Failed to create Window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, width, height);
+
+	glEnable(GL_DEPTH_TEST);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
